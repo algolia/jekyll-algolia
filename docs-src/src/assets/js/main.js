6534fe61828b55b38875f35d32cb0147698230e8
Pixelastic <tim@pixelastic.com>
2018-02-01 16:13:29 +0100

    Revert "Improve searchbar"
    This reverts commit 884a1ffdbf826bd9a95edcb5d04cd2d292c7a4a7.

--- docs-src/src/assets/js/main.js
+++ docs-src/src/assets/js/main.js
@@ -10,20 +10,8 @@ import { freezeAllGifs } from './freezeGifs.js';
 const docSearch = {
   apiKey: '5e2de32b362723ffdb03414c5c3d2ec8',
   indexName: 'jekyll_algolia',
-  inputSelector: '#searchbox'
+  inputSelector: '#searchbox',
 };
-
-
-document.querySelector('#searchbox').addEventListener('focus', function (e) {
-  document.querySelector('.algolia-autocomplete').classList.add('opened')
-});
-document.querySelector('#searchbox').addEventListener('blur', function (e) {
-  document.querySelector('.algolia-autocomplete').classList.remove('opened')
-});
-window.addEventListener('autocomplete:opened', function (e) {
-  document.querySelector('.algolia-autocomplete').classList.add('opened')
-});
-
 
 /* eslint-disable no-unused-vars */
 /* eslint-disable new-cap */
@@ -34,26 +22,6 @@ const codeSamples = document.querySelectorAll('.code-sample');
 
 activateClipboard(codeSamples);
 freezeAllGifs();
-
-const myImgs = document.querySelectorAll('.animate-me');
-
-const observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    if (entry.intersectionRatio > 0) {
-      [...myImgs].forEach(visual => {
-        visual.classList.add('animate')
-      })
-    } else {
-      [...myImgs].forEach(visual => {
-        visual.classList.remove('animate')
-      })
-    }
-  });
-});
-
-myImgs.forEach(image => {
-  observer.observe(image);
-});
 
 if (document.querySelector('.sidebar')) {
   repositionSidebarOnScroll();

79320b8f5d47c0aacb7c21cbc6dda1a67b7a2787
Pixelastic <tim@pixelastic.com>
2018-03-01 18:38:40 +0100

    test(integration): Make sure we can search for math symbols

--- spec/integration/index_spec.rb
+++ spec/integration/index_spec.rb
@@ -16,21 +16,38 @@ describe('pushed index') do
 
     context 'by default' do
       let(:distinct) { nil }
-      it { should eq 21 }
+      it { should eq 22 }
     end
     context 'with distinct:true' do
       let(:distinct) { true }
-      it { should eq 21 }
+      it { should eq 22 }
     end
     context 'with distinct:false' do
       let(:distinct) { false }
-      it { should eq 52 }
+      it { should eq 53 }
     end
   end
 
+  # https://github.com/algolia/jekyll-algolia/issues/49
   describe 'attributesToSnippet' do
-    # https://github.com/algolia/jekyll-algolia/issues/49
     subject { @index.get_settings['attributesToSnippet'] }
     it { should eq ['content:10'] }
+  end
+
+  # https://github.com/algolia/jekyll-algolia/issues/45
+  describe 'UTF-8 search' do
+    subject { @index.search(keyword)['hits'][0]['title'] }
+    context '∀' do
+      let(:keyword) { '∀' }
+      it { should eq 'Math symbols' }
+    end
+    context 'λ' do
+      let(:keyword) { 'λ' }
+      it { should eq 'Math symbols' }
+    end
+    context '→' do
+      let(:keyword) { '→' }
+      it { should eq 'Math symbols' }
+    end
   end
 end

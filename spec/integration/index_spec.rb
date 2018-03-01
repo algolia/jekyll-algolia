2d2598cc4609d9236e9e13476ea8267a1606b3fd
Pixelastic <tim@pixelastic.com>
2018-03-01 17:55:42 +0100

    test(integration): Check that attributesToSnippet is correctly set

--- spec/integration/index_spec.rb
+++ spec/integration/index_spec.rb
@@ -8,12 +8,11 @@ describe('pushed index') do
       application_id: ENV['ALGOLIA_APPLICATION_ID'],
       api_key: ENV['ALGOLIA_API_KEY']
     )
+    @index = Algolia::Index.new(ENV['ALGOLIA_INDEX_NAME'])
   end
 
-  let(:index) { Algolia::Index.new(ENV['ALGOLIA_INDEX_NAME']) }
-
   describe 'nbHits' do
-    subject { index.search('', distinct: distinct)['nbHits'] }
+    subject { @index.search('', distinct: distinct)['nbHits'] }
 
     context 'by default' do
       let(:distinct) { nil }
@@ -28,5 +27,10 @@ describe('pushed index') do
       it { should eq 52 }
     end
   end
-  # snippeting attribute correctly set
+
+  describe 'attributesToSnippet' do
+    # https://github.com/algolia/jekyll-algolia/issues/49
+    subject { @index.get_settings['attributesToSnippet'] }
+    it { should eq ['content:10'] }
+  end
 end

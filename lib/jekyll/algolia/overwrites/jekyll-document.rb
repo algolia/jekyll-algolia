45755de000c19e537d33736b796fe6adad5c419c
Haroen Viaene <hello@haroen.me>
2021-05-31 13:25:17 +0200

    fix(drafts): avoid error due to missing dates(#178)
    Resolves algolia/jekyll-algolia#177
    
    Co-Authored-By: leviem1 <leviem1@users.noreply.github.com>

--- lib/jekyll/algolia/overwrites/jekyll-document.rb
+++ lib/jekyll/algolia/overwrites/jekyll-document.rb
@@ -5,9 +5,10 @@ module Jekyll
   class Document
     # By default, Jekyll will set the current date (time of build) to any
     # collection item. This will break our diff algorithm, so we monkey patch
-    # this call to return nil if no date is defined instead.
+    # this call to return nil if no date is defined and the file is not a
+    # draft instead.
     def date
-      data['date'] || nil
+      data["date"] ||= (draft? ? source_file_mtime : nil)
     end
   end
 end
